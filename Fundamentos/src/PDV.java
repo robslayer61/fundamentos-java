46a0999bc5f05e09428eff798c3b3a9e77faaafc
robson.rsilva12 <robson.rsilva12@TAT0541243W10-1.senacsp.edu.br>
2019-06-03 22:23:47 -0300

    Adicionei Pedra, Papel e Tesoura

--- Fundamentos/src/PDV.java
+++ Fundamentos/src/PDV.java
@@ -1,16 +1,43 @@
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class PDV {
 
 	public static void main(String[] args) {
 		//Date --> trabalhar com data
+		// formatar a data
 		Date data = new Date();
-		//formatar a data
-		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
-		System.out.println(formatador.format(data));
-		
 
+		DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
+		DecimalFormat resultado = new DecimalFormat("0.00");
+		double total, desconto, totalDesconto, pago, troco;
+		Scanner leia = new Scanner(System.in);
+		
+		
+		System.out.println(formatador.format(data));
+		System.out.println("===============");
+		System.out.println("======PDV====");
+		System.out.println("===============");
+		System.out.println("");
+		
+		System.out.print("Valor Total: ");
+		desconto = leia.nextDouble();
+
+		System.out.print("Desconto em %:");
+		desconto = leia.nextDouble();
+		totalDesconto =  total - (desconto * total ) /100;
+		
+		System.out.println("Total com desconto: " + resultado.format(totalDesconto));
+		System.out.println("____________________________");
+		System.out.println("");
+		System.out.print("Valor pago: ");
+		pago = leia.nextDouble();
+		troco = pago - totalDesconto;
+		System.out.println("Troco: " + troco);
+		leia.close();
+		
 	}
 
 }
